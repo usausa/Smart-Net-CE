d4a6197fc69067c4026de2cafbe362fed4bbb25e
usausa <machi.pon@gmail.com>
2016-09-30 09:15:04 +0900

    Update version to 1.3.5

---
+++ Smart.CE/Functionals/Maybe.cs
@@ -0,0 +1,144 @@
+﻿namespace Smart.Functionals
+{
+    using System;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class Maybe
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static Maybe<T> Make<T>(T value) where T : class
+        {
+            return value == null ? Maybe<T>.None : new Maybe<T>(value);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static Maybe<T> Make<T>(T? value) where T : struct 
+        {
+            return value == null ? Maybe<T>.None : new Maybe<T>(value.Value);
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class Maybe<T>
+    {
+        internal static readonly Maybe<T> None = new Maybe<T>();
+        
+        private readonly T value;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public T Value
+        {
+            get { return value; }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool HasValue
+        {
+            get
+            {
+                return this != None;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        internal Maybe()
+        {
+            value = default(T);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        internal Maybe(T value)
+        {
+            this.value = value;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="defaultValue"></param>
+        /// <returns></returns>
+        public T GetOr(T defaultValue)
+        {
+            return HasValue ? Value : defaultValue;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="defaultValue"></param>
+        /// <returns></returns>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1062:ValidateArgumentsOfPublicMethods", Justification = "Performance")]
+        public T GetOr(Func<T> defaultValue)
+        {
+            return HasValue ? Value : defaultValue();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="action"></param>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1062:ValidateArgumentsOfPublicMethods", Justification = "Performance")]
+        public void IfPresent(Action<T> action)
+        {
+            if (HasValue)
+            {
+                action(Value);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="some"></param>
+        /// <param name="none"></param>
+        /// <returns></returns>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1062:ValidateArgumentsOfPublicMethods", Justification = "Performance")]
+        public TResult Match<TResult>(Func<T, TResult> some, Func<TResult> none)
+        {
+            return HasValue ? some(value) : none();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="some"></param>
+        /// <param name="none"></param>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1062:ValidateArgumentsOfPublicMethods", Justification = "Performance")]
+        public void Match(Action<T> some, Action none)
+        {
+            if (HasValue)
+            {
+                some(value);
+            }
+            else
+            {
+                none();
+            }
+        }
+    }
+}
--- Smart.CE/Properties/AssemblyInfo.cs
+++ Smart.CE/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
-[assembly: AssemblyTitle("Smart")]
+[assembly: AssemblyTitle("Smart.CE")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Smart")]
+[assembly: AssemblyProduct("Smart.CE")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 //
 // すべての値を指定するか、下のように '*' を使ってリビジョンおよびビルド番号を
 // 既定値にすることができます:
-[assembly: AssemblyVersion("1.3.4")]
+[assembly: AssemblyVersion("1.3.5")]
